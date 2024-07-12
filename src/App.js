31f9a996028009242e7af51859b0427e001a9a5d
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-07-13 00:12:39 +0600

    Update final socket url

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       className="App"
       style={{
         height:
-          window.location.href === "https://cool-media-fronend.netlify.app"
+          window.location.href === "https://cool-media-client.vercel.app"
             ? "calc(100vh - 2rem)"
             : "auto",
       }}
